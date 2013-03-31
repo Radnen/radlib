76064620fbcc125461a1914f9f6f5176d79d2a9e
Andrew <radnen@gmail.com>
2013-03-31 16:45:32 -0700

    Updated Parser
    Made a hand-rolled parser for testing

--- scripts/radscript/radlexer.js
+++ scripts/radscript/radlexer.js
@@ -7,61 +7,69 @@
 var RadLexer = (function() {
 	var code = [], current = 0,
 	    token = '', contents = "",
-			regs = [], lines = 1;
-	
+      regs = [], lines = 1, chars = 0;
+
 	function Open(filename) {
 		var file = OpenRawFile(filename);
 		contents = CreateStringFromByteArray(file.read(file.getSize()));
 		contents += "\n\0";
 		file.close();
 	}
-		
-	function Token(type, value) {
-		this.tok = type;
-		this.val = value;
-	}
 	
+	function Token(type, value) {
+		return {
+			tok: type,
+			val: value,
+			line: lines,
+			ch: chars
+		};
+	}
+
 	function Next() {
 		token = contents[current++];
 	}
-		
+	
 	function Peek() {
 		return contents[current];
 	}
-		
+
 	function Register(regex, token) {
 		if (Assert.is(token, "string")) token = token.charCodeAt(0);
 		regs.push({ r: regex, t: token });
 	}
-		
+
 	function Begin() {
-		code = []; current = 0; token = ''; lines = 1;
-		var str = "";
+		code = []; current = 0; token = ''; lines = 1; chars = 0;
+		var str = "", regex;
 		
 		Next();
 		while (token != '\0') {
-			if (token == '\n') lines++;
+			if (token == '\n') { chars = 0; lines++; }
 			
 			str += token;
 			for (var i = 0; i < regs.length; ++i) {
-				if (regs[i].r.test(str)) {
-					if (!regs[i].r.test(str + Peek())) {
-						if (regs[i].t > 0) code.push(new Token(regs[i].t, str));
-						if (regs[i].t == ERROR) Abort("Syntax Error: " + str + " on line " + lines);
+				regex = regs[i];
+				if ( regex.r.test( str ) ) {
+					if ( !regex.r.test( str + Peek() ) ) {
+						if ( regex.t > 0 ) code.push( Token(regex.t, str) );
+						
+						if ( regex.t == ERROR )
+							Abort("Syntax Error: " + str + " on line " + lines);
+						
 						str = "";
 					}
 					break;
 				}
 			}
 			Next();
+			chars++;
 		}
-		
-		Abort(code.toSource());
 	}
 	
 	function Lex(filename) {
 		Open(filename);
 		Begin();
+		return code;
 	}
 	
 	return {
--- scripts/radscript/radparser.js
+++ scripts/radscript/radparser.js
@@ -4,6 +4,109 @@
 * Updated: 3/31/2013
 **/
 
+/* PRESENTLY HAND-ROLLED */
+
 var RadParser = (function() {
+	var current = 0, token = null;
+			tokens = [], reg = [], stack = [];
 	
-});
+	function GetNextToken() {
+		token = tokens[current++];
+	}
+	
+	// 1 token look-ahead
+	function Peek() {
+		return tokens[current];
+	}
+	
+	function Ended() {
+		return current == tokens.length;
+	}
+	
+	function Print(expr) {
+		this.expr = expr;
+	}
+
+	Print.prototype.eval = function() {
+		Abort(this.expr.eval());
+	}
+
+	function BinExpr(type, left, right) {
+		this.t = type.tok;
+		this.left = left;
+		this.right = right;
+	}
+
+	BinExpr.prototype.eval = function() {
+		var l = this.left.eval();
+		var r = this.right.eval();
+		switch (this.t) {
+			case 43: return l + r;
+			case 45: return l - r;
+			case 42: return l * r;
+			case 47: return l / r;
+		}
+	}
+
+	function IntLit(type) {
+		this._int = Number(type.val);
+	}
+
+	IntLit.prototype.eval = function() {
+		return this._int;
+	}
+
+	function NextStmt() {
+		GetNextToken();
+		
+		if (token.tok == PRINT) {
+			GetNextToken();
+			return new Print(NextADD());
+		}
+	}
+	
+	function NextADD() {
+		var term = NextMUL();
+		
+		while (token) {
+			if (token.tok == 43 || token.tok == 45) {
+				var t = token;
+				GetNextToken();
+				term = new BinExpr(t, term, NextMUL());
+			}
+			else return term;
+		}
+	}
+	
+	function NextMUL() {
+		var term = NextNUM();
+		
+		while(token) {
+			if (token.tok == 42 || token.tok == 47) {
+				var t = token;
+				GetNextToken();
+				term = new BinExpr(t, term, NextNUM());
+			}
+			else return term;
+		}
+	}
+	
+	function NextNUM() {
+		if (token.tok == NUM) {
+			var t = token;
+			GetNextToken();
+			return new IntLit(t);
+		}
+	}
+	
+	function Parse(toks) {
+		token = null; current = 0;
+		tokens = toks; reg = []; stack = [];
+		toks.push( {tok: '$'.charCodeAt(0)} );
+		return NextStmt();
+	}
+	
+	return {
+		parse: Parse
+	}
+}());
